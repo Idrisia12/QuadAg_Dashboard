eec471dafaa15b93ccd0590f1c8ea8d5e64ef574
Abdulrasheed Idris <66267919+Idrisia12@users.noreply.github.com>
2020-10-27 17:23:51 +0100

    Update retail.js

--- src/components/retail.js
+++ src/components/retail.js
@@ -15,13 +15,13 @@ const layout = {
 };
 
 const validateMessages = {
-  required: '${label} is required!',
+  required: `${label} is required!`,
   types: {
-    email: '${label} is not validate email!',
-    number: '${label} is not a validate number!',
+    email: `${label} is not validate email!`,
+    number: `${label} is not a validate number!`,
   },
   number: {
-    range: '${label} must be between ${min} and ${max}',
+    range: `${label} must be between ${min} and ${max}`,
   },
 };
 
