b0f3dfb2d6b735d691345f8f3af883c3f3555ef3
Abdulrasheed Idris <66267919+Idrisia12@users.noreply.github.com>
2020-10-28 09:21:26 +0100

    Update retail.js

--- src/components/retail.js
+++ src/components/retail.js
@@ -16,13 +16,13 @@ const layout = {
 
 /*eslint no-template-curly-in-string: "error"*/
 const validateMessages = {
-  required: `${label} is required!`, 
+  required: "${label} is required!", 
   types: {
-    email: `${label} is not validate email!`, 
-    number: `${label} is not a validate number!`, 
+    email: "${label} is not validate email!", 
+    number: "${label} is not a validate number!", 
   },
   number: {
-    range: `${label} must be between ${min} and ${max}`, 
+    range: "${label} must be between ${min} and ${max}", 
   },
 };
 
