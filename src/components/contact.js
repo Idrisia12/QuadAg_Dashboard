575a8ec881eca543b3032cca8b321574b5565420
Abdulrasheed Idris <66267919+Idrisia12@users.noreply.github.com>
2020-10-29 09:55:27 +0100

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -16,18 +16,6 @@ const layout = {
   wrapperCol: { span: 16 },
 };
 
-/*eslint no-template-curly-in-string: "error"*/
-const validateMessages = {
-  required: "`${label}` is required!",
-  types: {
-    email: "`${label}` is not validate email!", 
-    number: "`${label}` is not a validate number!",
-  },
-  number: {
-    range: "`${label}` must be between `${min}` and `${max}`", 
-  },
-};
-
 const SeeaDoctor = () => {
   const onFinish = values => {
     console.log(values);
@@ -39,11 +27,11 @@ const SeeaDoctor = () => {
        <Title level={2} style={{ textAlign: 'center'}}>Contact Us</Title>
        <p style={{ textAlign: 'center'}}>You can reach us via the following channels.<span style={{ color: "#32be8f" }}>We will try to reply as soon as possible</span></p>
 
-       <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-            <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+       <Form {...layout} name="nest-messages" onFinish={onFinish}>
+            <Form.Item name={['user', 'name']} rules={[{ required: true }]}>
               <Input />
             </Form.Item>
-            <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email', required: true}]}>
+            <Form.Item name={['user', 'email']} rules={[{ type: 'email', required: true}]}>
               <Input />
             </Form.Item>
             <Form.Item name={['user', 'introduction']} label="Message">
